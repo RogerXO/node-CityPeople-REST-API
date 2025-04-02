564ef319cacebcee61edc45692001e9432d49115
RogerXavier <rogerxavier10@gmail.com>
2025-04-02 20:19:12 -0300

    feat: yup validating multiple errors

--- src/server/controllers/cities/create.ts
+++ src/server/controllers/cities/create.ts
@@ -1,25 +1,32 @@
 import { Request, Response } from "express";
 import { ICity } from "../../models/cities.models";
-import * as yup from "yup"
+import * as yup from "yup";
 import { StatusCodes } from "http-status-codes";
 
 const bodyValidation: yup.ObjectSchema<ICity> = yup.object().shape({
-  name: yup.string().required().min(3).max(100)
-})
+  name: yup.string().required().min(3).max(100),
+});
 
 export async function create(req: Request<{}, {}, ICity>, res: Response) {
-  let validatedData: ICity | undefined = undefined
+  let validatedData: ICity | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body)
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
   } catch (error) {
-    const yupError = error as yup.ValidationError
+    const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string, string> = {};
+
+    yupError.inner.forEach((err) => {
+      if (!err.path) return;
+
+      validationErrors[err.path] = err.message;
+    });
 
     return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: {
-        default: yupError.message
-      }
-    })
+      errors: validationErrors,
+    });
   }
 
   return res.send("City created");
