975bb7f28cec3f7632c98bd671e32c16ec384e0b
Roger Xavier <rogerxavier10@gmail.com>
2025-05-19 18:15:08 -0300

    fix: digital ocean prod env fixed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/server";
 
 const port = process.env.PORT ?? 3333;
 
-server.listen(port, () =>
-  console.log(`App running at http://localhost:${port}`)
-);
+function startServer() {
+  server.listen(port, () =>
+    console.log(`App running at http://localhost:${port}`)
+  );
+}
+
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate
+    .latest()
+    .then(() => startServer())
+    .catch(console.log);
+} else {
+  startServer()
+}
